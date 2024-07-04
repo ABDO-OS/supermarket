bfeafa183db60aa0deb3c7877cbfe489b85b4654
Ali Abdelmoaty <68239507+aliabdelmoaty@users.noreply.github.com>
2024-07-04 20:25:43 +0300

    refactor: Update login screen UI with reusable widgets and consistent styles

--- lib/feature/login/ui/login_screen.dart
+++ lib/feature/login/ui/login_screen.dart
@@ -1,8 +1,13 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:supermarket/core/theme/colors.dart';
 import 'package:supermarket/core/theme/styles.dart';
+import 'package:supermarket/core/widgets/button_app.dart';
+import 'package:supermarket/core/widgets/text_rich.dart';
+import 'package:supermarket/feature/login/ui/widgets/forget_password.dart';
+import 'package:supermarket/feature/login/ui/widgets/widget_text_from_field.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -27,89 +32,29 @@ class LoginScreen extends StatelessWidget {
               Text(
                 'Enter your emails and password',
                 textAlign: TextAlign.center,
-                style: TextStylesApp.textStyleMedium16,
+                style: TextStylesApp.textStyleRegular16,
               ),
               SizedBox(height: 35.h),
               const WidgetTextFromField(),
               SizedBox(height: 8.h),
-              Align(
-                alignment: Alignment.centerRight,
-                child: TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    'Forgot Password?',
-                    style: TextStylesApp.textStyleMedium14,
-                  ),
-                ),
+              const ForgetPassword(),
+              SizedBox(height: 20.h),
+              const ButtonApp(
+                text: 'Log In',
               ),
+              SizedBox(
+                height: 15.h,
+              ),
+              const Center(
+                child: TextRich(
+                  text1: 'Don’t have an account? ',
+                  text2: 'Sing up',
+                ),
+              )
             ],
           ),
         ),
       ),
     ));
-  }
-}
-
-class CustomTextFormField extends StatelessWidget {
-  final TextEditingController controller;
-  final String? labelText;
-  const CustomTextFormField({
-    super.key,
-    required this.controller,
-    this.labelText,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return TextFormField(
-      controller: controller,
-      style: TextStylesApp.textStyleMedium18,
-      cursorColor: ColorApp.secondary,
-      keyboardType: TextInputType.emailAddress,
-      decoration: InputDecoration(
-          labelText: labelText,
-          labelStyle: TextStylesApp.textStyleSemi16,
-          suffixIcon: labelText == 'Password'
-              ? IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.visibility,
-                    color: ColorApp.secondary,
-                  ),
-                )
-              : null,
-          focusedBorder: UnderlineInputBorder(
-            borderSide: BorderSide(color: ColorApp.secondary),
-          )),
-    );
-  }
-}
-
-class WidgetTextFromField extends StatefulWidget {
-  const WidgetTextFromField({super.key});
-
-  @override
-  State<WidgetTextFromField> createState() => _WidgetTextFromFieldState();
-}
-
-class _WidgetTextFromFieldState extends State<WidgetTextFromField> {
-  TextEditingController emailController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        CustomTextFormField(
-          controller: emailController,
-          labelText: 'Email',
-        ),
-        SizedBox(height: 20.h),
-        CustomTextFormField(
-          controller: passwordController,
-          labelText: 'Password',
-        ),
-      ],
-    );
   }
 }
