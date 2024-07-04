eb105d6b58ff482badebe72a08b62f8cfa970b59
Ali Abdelmoaty <68239507+aliabdelmoaty@users.noreply.github.com>
2024-07-04 20:24:37 +0300

    feat: Add colorButton and colorButtonText constants to ColorApp

--- lib/core/theme/colors.dart
+++ lib/core/theme/colors.dart
@@ -3,4 +3,6 @@ import 'package:flutter/material.dart';
 abstract class ColorApp{
   static Color primary = const Color(0xFF181725);
   static Color secondary = const Color(0xFF7C7C7C);
+  static Color colorButton = const Color(0xFF53B175);
+  static Color colorButtonText = const Color(0xFFFFF9FF);
 }
